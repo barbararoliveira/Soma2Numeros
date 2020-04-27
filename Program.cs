e16f484f0ccda7f4a498eb696c3669cd26e6812d
Bárbara <babyr.oliveira@gmail.com>
2020-04-27 13:35:56 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace soma2numeross
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Olá usuário, vou ajudá-lo a somar."); 
+          Console.WriteLine("Pressione enter para continuar.");
+
+          Console.ReadKey ();
+
+          Console.WriteLine("Digite um número inteiro."); 
+          int valor1 = Int32.Parse (Console.ReadLine ()); 
+
+          Console.WriteLine("Digite outro número inteiro");
+          int valor2 = Int32.Parse (Console.ReadLine ());
+
+          int resultadodasoma = valor1 + valor2; 
+          Console.WriteLine ("Resultado da soma:" + resultadodasoma);
+          
+          Console.ReadKey ();
+        }
+    }
+}
